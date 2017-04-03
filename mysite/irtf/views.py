93d3e8046b6395d409872c19cea05c5b9e12792f
Alexa Villaume <avillaum@ucsc.edu>
2017-04-03 15:59:44 -0400

    working on adding forms where users can enter information and retrieve data products

--- mysite/irtf/views.py
+++ mysite/irtf/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Targets, EquivWidths
+from .models import Targets
 
 import csv
 import numpy as np
 
 def index(request):
+
+    try:
+        star = Targets.objects.filter(name='BD+002058A')
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="irtf_test.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(star)
+        return response
+    except (KeyError, Target.DoesNotExist):
+        return render(request, {
+            'error_message': "That star doesn't exist"
+        })
+
     names = Targets.objects.order_by('name')
 
     template = loader.get_template('irtf/index.html')
