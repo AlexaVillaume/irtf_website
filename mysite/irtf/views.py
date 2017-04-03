00bc99551ec0a0fd7c0c859aa6e38f34572598af
Alexa Villaume <avillaum@ucsc.edu>
2017-04-03 15:37:13 -0400

    Adding in templates to format the page

--- mysite/irtf/views.py
+++ mysite/irtf/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Targets, EquivWidths
+
+import csv
+import numpy as np
+
+def index(request):
+    names = Targets.objects.order_by('name')
+
+    template = loader.get_template('irtf/index.html')
+    context = {
+                'output': names,
+            }
+
+    return HttpResponse(template.render(context, request))
+
+
+def test_write_csv(request):
+    names = Targets.objects.order_by('name')[:5]
+    output = ', '.join([str(n) for n in names])
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="irtf_test.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(output)
+    return response
+
