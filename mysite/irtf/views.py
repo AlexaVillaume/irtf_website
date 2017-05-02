9f7e4518e2eeec32045341ed59b247993d26ff9b
Alexa Villaume <avillaum@ucsc.edu>
2017-05-01 18:46:06 -0700

    Added metallicity to data form. Cleaning up explore page

--- mysite/irtf/views.py
+++ mysite/irtf/views.py
@@ -18,53 +18,62 @@ def index(request):
 
         return HttpResponse(template.render(request))
 
+def test():
+    pass
+
 def explore(request):
     try:
         teff_min = request.POST['teff_min']
         teff_max = request.POST['teff_max']
+        feh_min = request.POST['feh_min']
+        feh_max = request.POST['feh_max']
         logg_min = request.POST['logg_min']
         logg_max = request.POST['logg_max']
-
-
     except:
         return render(request, 'irtf/explore.html', {
             'error_message': "Something is not right"
             })
-
     else:
         form = ExploreForm(request.POST)
-        stars = Targets.objects.filter(teff__range=(teff_min, teff_max),
-                                       logg__range=(logg_min, logg_max)).exclude(
-                                        irtf_spec__isnull=True)
+        stars = get_data(teff_min, teff_max, feh_min,
+                         feh_max, logg_min, logg_max)
+
         download = DownloadForm(initial={
                 'teff_min': teff_min,
                 'teff_max': teff_max,
+                'feh_min': feh_min,
+                'feh_max': feh_max,
                 'logg_min': logg_min,
                 'logg_max': logg_max,
                 })
 
+
         context = {
-                    'form': form,
-                    'output': stars,
-                    'download_form': download
+                'form': form,
+                'output': stars,
+                'download_form': download
                 }
+
         template = loader.get_template('irtf/explore.html')
 
-        return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))
 
 
-def get_data(teff_min, teff_max, logg_min, logg_max):
+def get_data(teff_min, teff_max, feh_min, feh_max, logg_min, logg_max):
     return Targets.objects.filter(teff__range=(teff_min, teff_max),
-                                  logg__range=(logg_min, logg_max)).exclude(
-                                          irtf_spec__isnull=True)
+                                  logg__range=(logg_min, logg_max),
+                                  fe_h__range=(feh_min, feh_max)).exclude(
+                                          irtf_spec__isnull=False)
 
 def download_data(request):
 
     teff_min = request.POST['teff_min']
     teff_max = request.POST['teff_max']
+    feh_min = request.POST['feh_min']
+    feh_max = request.POST['feh_max']
     logg_min = request.POST['logg_min']
     logg_max = request.POST['logg_max']
-    stars = get_data(teff_min, teff_max, logg_min, logg_max)
+    stars = get_data(teff_min, teff_max, feh_min, feh_max, logg_min, logg_max)
 
 
     data = serializers.serialize('json', stars)
